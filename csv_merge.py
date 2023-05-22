27e70399eae3f293e240633b930da1883b5b04ae
maneskold <130664667+maneskold@users.noreply.github.com>
2023-05-22 13:21:38 +0200

    Add files via upload

--- csv_merge.py
+++ csv_merge.py
@@ -1,12 +1,7 @@
+import csv
 
-import pandas as pd
+with open('2022.csv', 'r') as csv_file:
+    reader = csv.reader(csv_file)
 
-file1 = "/Users/lisamaneskold/Downloads/2021.csv"
-file2 = "/Users/lisamaneskold/Downloads/2022.csv"
-
-print("Merging multiple CSV files...")
-
-# merge
-dataFrame = pd.concat(
-   map(pd.read_csv, [file1, file2]), ignore_index=True)
-print(dataFrame)
+    for row in reader:
+        print(row)
