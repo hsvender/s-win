b01d6e80ad284ab567788fe27cc218ba2d0dff16
hsvender <gussvehaah@student.gu.se>
2023-05-24 12:39:14 +0200

    Updates

--- swin/swinapp/static/swinapp/js/script.js
+++ swin/swinapp/static/swinapp/js/script.js
@@ -1,5 +1,34 @@
-console.log("Hello world!");
+// Fetch the JSON data (assuming it's in a file called data.json)
+fetch('data/comp_prog_count.json')
+  .then(response => response.json())
+  .then(data => {
+    // Sort the attributes based on their values in descending order
+    const sortedAttributes = Object.entries(data).sort((a, b) => b[1] - a[1]);
 
-function toggleMenu() {
-	var menu
-}
+    // Get the visualization container element
+    const visualizationContainer = document.getElementById('visualization');
+
+    // Loop through the sorted attributes and create bars for each attribute
+    sortedAttributes.forEach(([attribute, value]) => {
+      // Create a bar element
+      const bar = document.createElement('div');
+      bar.classList.add('bar');
+
+      // Calculate the width of the bar based on the attribute value
+      const width = (value / sortedAttributes[0][1]) * 100; // Adjust the denominator if needed
+
+      // Create a span element within the bar to represent the attribute value visually
+      const span = document.createElement('span');
+      span.style.width = `${width}%`;
+
+      // Add the attribute name as text content of the bar
+      bar.textContent = attribute;
+
+      // Append the span to the bar
+      bar.appendChild(span);
+
+      // Append the bar to the visualization container
+      visualizationContainer.appendChild(bar);
+    });
+  })
+  .catch(error => console.error('Error:', error));
